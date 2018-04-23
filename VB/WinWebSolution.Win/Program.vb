f0e02bd8d517fce4a9321e358b8f0f4d1da1b4d1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WinWebSolution.Win/Program.vb
@@ -0,0 +1,47 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Configuration
+Imports System.Windows.Forms
+
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Security
+Imports DevExpress.ExpressApp.Win
+Imports DevExpress.Persistent.Base
+Imports DevExpress.Persistent.BaseImpl
+Imports Demo
+
+Namespace WinWebSolution.Win
+	Friend NotInheritable Class Program
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		Private Sub New()
+		End Sub
+		<STAThread> _
+		Shared Sub Main()
+#If EASYTEST Then
+			DevExpress.ExpressApp.EasyTest.WinAdapter.RemotingRegistration.Register(4100)
+#End If
+
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached
+			Dim winApplication As New WinWebSolutionWindowsFormsApplication()
+#If EASYTEST Then
+			If ConfigurationManager.ConnectionStrings("EasyTestConnectionString") IsNot Nothing Then
+				winApplication.ConnectionString = ConfigurationManager.ConnectionStrings("EasyTestConnectionString").ConnectionString
+			End If
+#End If
+			winApplication.ConnectionString = CodeCentralExampleInMemoryDataStoreProvider.ConnectionString
+			If ConfigurationManager.ConnectionStrings("ConnectionString") IsNot Nothing Then
+				winApplication.ConnectionString = ConfigurationManager.ConnectionStrings("ConnectionString").ConnectionString
+			End If
+			Try
+				winApplication.Setup()
+				winApplication.Start()
+			Catch e As Exception
+				winApplication.HandleException(e)
+			End Try
+		End Sub
+	End Class
+End Namespace
