c0a85f1353aa047ac4fa50b6927f5f8718162b05
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WinWebSolution.Module/Updater.vb
@@ -0,0 +1,28 @@
+Imports Microsoft.VisualBasic
+Imports System
+
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.Xpo
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Persistent.BaseImpl
+Imports WinSolution.Module
+
+Namespace WinWebSolution.Module
+	Public Class Updater
+		Inherits ModuleUpdater
+		Public Sub New(ByVal objectSpace As DevExpress.ExpressApp.IObjectSpace, ByVal currentDBVersion As Version)
+			MyBase.New(objectSpace, currentDBVersion)
+		End Sub
+		Public Overrides Sub UpdateDatabaseAfterUpdateSchema()
+			MyBase.UpdateDatabaseAfterUpdateSchema()
+			Dim dept As New Department(Session)
+			dept.Title = "Demo Department"
+			Dim le As New LocalEmployee(Session)
+			le.Name = "LocalEmployee 1"
+			Dim fe As New ForeignEmployee(Session)
+			fe.Name = "ForeignEmployee 1"
+			dept.Employees.AddRange(New EmployeeBase() { le, fe })
+			dept.Save()
+		End Sub
+	End Class
+End Namespace
